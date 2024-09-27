988edf5bdd4b3d6a119da42c9a6b6f02539adbea
Hx2 <81757443+devhx2@users.noreply.github.com>
2024-09-27 16:53:11 +0900

    setFont,drawBack.drawTextを追加

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <string>
 
+constexpr int Width = 1920;
+constexpr int Height = 1080;
+constexpr COLORREF Black = RGB(0, 0, 0);
+constexpr COLORREF Green = RGB(19, 161, 14);
+
 HWND getWorkerW()
 {
     // デスクトップ画面を管理するウィンドウを取得
@@ -50,14 +55,41 @@ void setWallpaper(const std::string path)
     std::cout << "Set Wallpaper: ";
 
     // デスクトップの壁紙を設定
-    if (SystemParametersInfo(SPI_SETDESKWALLPAPER, NULL, (PVOID)path.c_str(), SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)) std::cout << "OK" << std::endl;
+    if (SystemParametersInfo(SPI_SETDESKWALLPAPER, NULL, (PVOID)path.c_str(),
+        SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)) std::cout << "OK" << std::endl;
     else std::cout << "Error" << std::endl;
 }
 
-constexpr int Width = 1920;
-constexpr int Height = 1080;
-constexpr COLORREF Black = RGB(0, 0, 0);
-constexpr COLORREF Green = RGB(19, 161, 14);
+void setFont(const HDC hdc)
+{
+    const HFONT font = CreateFont(20, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE,
+                                  ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, 
+                                  DRAFT_QUALITY, DEFAULT_PITCH, "Cascadia Mono SemiBold");
+    SelectObject(hdc, font);
+    DeleteObject(font);
+}
+
+void drawBack(const HDC hdc)
+{
+    const HBRUSH brush = CreateSolidBrush(Black);
+
+    SelectObject(hdc, brush);
+
+    // WorkerW一面をbrushで塗りつぶす
+    Rectangle(hdc, 0, 0, Width, Height);
+
+    DeleteObject(brush);
+}
+
+void drawText(const HDC hdc, int x, int y, const COLORREF color, const std::string text)
+{
+    // 文字の後ろは塗りつぶさない
+    SetBkMode(hdc, TRANSPARENT);
+
+    SetTextColor(hdc, color);
+
+    TextOut(hdc, x, y, text.c_str(), strlen(text.c_str()));
+}
 
 int main()
 {
@@ -68,27 +100,12 @@ int main()
     if (path == "") return -1;
 
     const HDC hdc = GetDC(workerW);
-    const HBRUSH brush = CreateSolidBrush(Black);
-    SelectObject(hdc, brush);
 
-    // WorkerW一面をbrushで塗りつぶす
-    Rectangle(hdc, 0, 0, Width, Height);
+    setFont(hdc);
 
-    const HFONT font = CreateFont(20, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET,
-                                  OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DRAFT_QUALITY, DEFAULT_PITCH, "Cascadia Mono SemiBold");
-    SelectObject(hdc, font);
+    drawBack(hdc);
+    drawText(hdc, 0, 0, Green, "Text");
 
-    // 文字の後ろは塗りつぶさない
-    SetBkMode(hdc, TRANSPARENT);
-
-    const wchar_t* text = L"Test";
-
-    SetTextColor(hdc, Green);
-
-    TextOutW(hdc, 0, 0, text, wcslen(text));
-
-    DeleteObject(brush);
-    DeleteObject(font);
     ReleaseDC(workerW, hdc);
 
     std::cin.get();
