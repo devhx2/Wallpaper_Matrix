09e38479b23b5dabf8c6c4328fa1fc7a5c974291
Hx2 <81757443+devhx2@users.noreply.github.com>
2024-09-27 15:26:45 +0900

    定数化

--- main.cpp
+++ main.cpp
@@ -54,6 +54,11 @@ void setWallpaper(const std::string path)
     else std::cout << "Error" << std::endl;
 }
 
+constexpr int Width = 1920;
+constexpr int Height = 1080;
+constexpr COLORREF Black = RGB(0, 0, 0);
+constexpr COLORREF Green = RGB(19, 161, 14);
+
 int main()
 {
     const HWND workerW = getWorkerW();
@@ -62,44 +67,44 @@ int main()
     const std::string path = getWallpaper();
     if (path == "") return -1;
 
-    //HDC hdc = GetDC(workerW);
+    const HDC hdc = GetDC(workerW);
+    const HBRUSH brush = CreateSolidBrush(Black);
 
-    //HBRUSH brush = CreateSolidBrush(RGB(0, 0, 0));
-    //SelectObject(hdc, brush);
-    //Rectangle(hdc, 0, 0, 1920, 1080);
+    SelectObject(hdc, brush);
+    Rectangle(hdc, 0, 0, Width, Height);
 
-    //HFONT font = CreateFont(
-    //    20,                            // フォントの高さ(大きさ)。
-    //    0,                             // フォントの幅。普通０。
-    //    0,                             // 角度。０でＯＫ。
-    //    0,                             // 同じく角度。これも０。
-    //    FW_DONTCARE,                   // 文字の太さ。
-    //    FALSE,                         // フォントがイタリックならTRUEを指定。
-    //    FALSE,                         // 下線を引くならTRUE。
-    //    FALSE,                         // 取り消し線を引くならTRUE。
-    //    ANSI_CHARSET,                  // フォントの文字セット。このままでＯＫ。
-    //    OUT_DEFAULT_PRECIS,            // 出力精度の設定。このままでＯＫ。
-    //    CLIP_DEFAULT_PRECIS,           // クリッピング精度。このままでＯＫ。
-    //    DRAFT_QUALITY,                 // フォントの出力品質。このままでＯＫ。
-    //    DEFAULT_PITCH,                 // フォントのピッチとファミリを指定。このままでＯＫ。
-    //    TEXT("Cascadia Mono SemiBold") // フォントのタイプフェイス名の指定。これは見たまんま。
-    //);
+    HFONT font = CreateFont(
+        20,                            // フォントの高さ(大きさ)。
+        0,                             // フォントの幅。普通０。
+        0,                             // 角度。０でＯＫ。
+        0,                             // 同じく角度。これも０。
+        FW_DONTCARE,                   // 文字の太さ。
+        FALSE,                         // フォントがイタリックならTRUEを指定。
+        FALSE,                         // 下線を引くならTRUE。
+        FALSE,                         // 取り消し線を引くならTRUE。
+        ANSI_CHARSET,                  // フォントの文字セット。このままでＯＫ。
+        OUT_DEFAULT_PRECIS,            // 出力精度の設定。このままでＯＫ。
+        CLIP_DEFAULT_PRECIS,           // クリッピング精度。このままでＯＫ。
+        DRAFT_QUALITY,                 // フォントの出力品質。このままでＯＫ。
+        DEFAULT_PITCH,                 // フォントのピッチとファミリを指定。このままでＯＫ。
+        "Cascadia Mono SemiBold"       // フォントのタイプフェイス名の指定。これは見たまんま。
+    );
 
-    //SelectObject(hdc, font);
+    SelectObject(hdc, font);
 
-    //const wchar_t* text = L"Test";
+    const wchar_t* text = L"Test";
 
-    //SetBkMode(hdc, TRANSPARENT);
+    SetBkMode(hdc, TRANSPARENT);
 
-    //SetTextColor(hdc, RGB(19, 161, 14));
-    //TextOutW(hdc, 100, 100, text, wcslen(text));
+    SetTextColor(hdc, Green);
+    TextOutW(hdc, 100, 100, text, wcslen(text));
 
-    //DeleteObject(brush);
-    //DeleteObject(font);
+    DeleteObject(brush);
+    DeleteObject(font);
 
-    //ReleaseDC(workerW, hdc);
+    ReleaseDC(workerW, hdc);
 
-    //std::cin.get();
+    std::cin.get();
 
     setWallpaper(path);
 
