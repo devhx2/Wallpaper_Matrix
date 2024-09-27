9417a0b958bc72e05042d1b6c03da1f84b74505f
Hx2 <81757443+devhx2@users.noreply.github.com>
2024-09-27 14:40:21 +0900

    テストコードを移植

--- Wallpaper_Matrix.cpp
+++ Wallpaper_Matrix.cpp
@@ -1,6 +1,66 @@
-﻿#include <iostream>
+﻿#include <windows.h>
+
+#include <functional>
+#include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HWND progman = FindWindow("Progman", NULL);
+    std::cout << "Progman:" << progman << std::endl;
+
+    SendMessageTimeout(progman, 0x052C, NULL, NULL, SMTO_NORMAL, 1000, NULL);
+
+    HWND workerW = GetNextWindow(progman, GW_HWNDPREV);
+
+    std::cout << "WorkerW:" << workerW << std::endl;
+
+    char path[MAX_PATH]{};
+    SystemParametersInfo(SPI_GETDESKWALLPAPER, MAX_PATH, path, 0);
+    std::cout << "Path:" << path << std::endl;
+
+    HDC hdc = GetDC(workerW);
+
+    HBRUSH brush = CreateSolidBrush(RGB(0, 0, 0));
+    SelectObject(hdc, brush);
+    Rectangle(hdc, 0, 0, 1920, 1080);
+
+    HFONT font = CreateFont(
+        20,                            // フォントの高さ(大きさ)。
+        0,                             // フォントの幅。普通０。
+        0,                             // 角度。０でＯＫ。
+        0,                             // 同じく角度。これも０。
+        FW_DONTCARE,                   // 文字の太さ。
+        FALSE,                         // フォントがイタリックならTRUEを指定。
+        FALSE,                         // 下線を引くならTRUE。
+        FALSE,                         // 取り消し線を引くならTRUE。
+        ANSI_CHARSET,                  // フォントの文字セット。このままでＯＫ。
+        OUT_DEFAULT_PRECIS,            // 出力精度の設定。このままでＯＫ。
+        CLIP_DEFAULT_PRECIS,           // クリッピング精度。このままでＯＫ。
+        DRAFT_QUALITY,                 // フォントの出力品質。このままでＯＫ。
+        DEFAULT_PITCH,                 // フォントのピッチとファミリを指定。このままでＯＫ。
+        TEXT("Cascadia Mono SemiBold") // フォントのタイプフェイス名の指定。これは見たまんま。
+    );
+
+    SelectObject(hdc, font);
+
+    const wchar_t* text = L"Test";
+
+    SetBkMode(hdc, TRANSPARENT);
+
+    SetTextColor(hdc, RGB(19, 161, 14));
+    TextOutW(hdc, 100, 100, text, wcslen(text));
+
+    DeleteObject(brush);
+    DeleteObject(font);
+
+    ReleaseDC(workerW, hdc);
+
+    std::cin.get();
+
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, NULL, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+
+    std::cin.get();
+
+    return 0;
 }
